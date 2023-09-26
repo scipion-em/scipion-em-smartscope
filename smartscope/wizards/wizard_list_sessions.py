2e58aafebc0315670e3ce3e7274bd531a68a99d0
albertmena <12760268+albertmena@users.noreply.github.com>
2023-09-26 10:51:26 +0200

    improving wizard

--- smartscope/wizards/wizard_list_sessions.py
+++ smartscope/wizards/wizard_list_sessions.py
@@ -18,13 +18,15 @@ class smartscopeWizard(Wizard):
         for s in reversed(sessionList):
             if sessionOpen:
                 if sessionOpen == s.getSessionId() and lastFound == False:
-                    live = '*last session started*'
+                    live = '  *last session started*'
                     lastFound = True
             date = datetime.datetime.strptime(s.getDate(),'%Y%M%d')
-            sList.append(['{}  \tDate: {}-{}-{}\t{}'.format(s.getSession(),
+
+            sList.append(['{}-{}-{}\t{}{}'.format(
                                             date.strftime("%Y"),
                                             date.strftime("%M"),
-                                            date.strftime("%d"), live),
+                                            date.strftime("%d"),s.getSession(),
+                                            live),
                          s.getSession()])
             live = ''
 
@@ -34,7 +36,12 @@ class smartscopeWizard(Wizard):
         provider = ListTreeProviderString(finalList)
         dlg = dialog.ListDialog(form.root, "Sessions available", provider,
         "Select the session")
-        name = dlg.values[0].get()[:dlg.values[0].get().find('  \t')]
-        for s in sList:
-            if name in s[0]:
-                form.setVar('sessionName', name)
+        try:
+            OutStr = dlg.values[0].get()
+            OutStr = OutStr.replace('  *last session started*', '')
+            name = OutStr[OutStr.find('\t')+1:]
+            for s in sList:
+                if name in s[0]:
+                    form.setVar('sessionName', name)
+        except IndexError:#Nos selection
+            pass
