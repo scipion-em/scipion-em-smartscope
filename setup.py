d9809c8c6bb8f986e7afd79d77829ab0e131a7ce
Pablo Conesa <p.conesa.mingo@gmail.com>
2023-09-28 12:28:29 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ from setuptools import setup, find_packages
 # To use a consistent encoding
 from codecs import open
 from os import path
+from smartscope import __version__
 
 here = path.abspath(path.dirname(__file__))
 
@@ -25,7 +26,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='scipion-em-smartscope',  # Required
-    version='0.1',  # Required
+    version=__version__,  # Required
     description='Plugin to manage Smartscope from Scipion',  # Required
     long_description=long_description,  # Optional
     url='https://github.com/scipion-em/scipion-em-smartscope',  # Optional
