3b34fbc98b371167382cfc6f4c0058443f7c6b98
albertmena <alber.mena@gmail.com>
2024-06-11 10:55:53 +0000

    test

--- smartscope/protocols/protocol_feedback_filter.py
+++ smartscope/protocols/protocol_feedback_filter.py
@@ -57,8 +57,6 @@ class smartscopeFeedbackFilter(ProtImport, ProtStreamingBase):
     _devStatus = BETA
     _possibleOutputs = {'SetOfHoles': SetOfHoles}
 
-
-
     def __init__(self, **args):
         ProtImport.__init__(self, **args)
         self.stepsExecutionMode = STEPS_PARALLEL
