46aa0e50ab9d6d9274c952d0e22e53669aedbc2f
albertmena <alberto.garcia@cnb.csic.es>
2024-10-11 10:44:32 +0000

    fixing viewer

--- smartscope/protocols/protocol_feedback_filter.py
+++ smartscope/protocols/protocol_feedback_filter.py
@@ -44,18 +44,18 @@ from pyworkflow.protocol import params, STEPS_PARALLEL
 from ..objects.dataCollection import *
 import time
 from ..constants import *
-
+from scipy.stats import shapiro
 
 
 class smartscopeFeedbackFilter(ProtImport, ProtStreamingBase):
     """
     This protocol will calculate which are the best holes of the session based
-    on the good particles of each hole. After knowing the good holes, will
-    sort the queue of hole acquisition that Smartscope uses.
+    on the micrographs filtered by alignment, CTF estimations.... After knowing the good holes, will
+    send the range of intensities of hole that Smartscope uses.
     """
     _label = 'Feedback filter'
     _devStatus = BETA
-    _possibleOutputs = {'SetOfHoles': SetOfHoles}
+    _possibleOutputs = {'SetOfHolesFiltered': SetOfHoles, 'SetOfHolesFilteredOut': SetOfHoles}
 
     def __init__(self, **args):
         ProtImport.__init__(self, **args)
@@ -67,6 +67,7 @@ class smartscopeFeedbackFilter(ProtImport, ProtStreamingBase):
 
         self.pyClient = MainPyClient(self.token, self.endpoint)
         self.connectionClient = dataCollection(self.pyClient)
+        self.countStreamingSteps = 0
 
     def _defineParams(self, form):
         """ Define the input parameters that will be used.
@@ -92,18 +93,26 @@ class smartscopeFeedbackFilter(ProtImport, ProtStreamingBase):
                       important=True, allowsNull=False,
                       label='Filtered micrographs',
                       help='Select a set of micrographs filtered by any protocol.')
-        form.addParam('triggerMicrograph', params.IntParam, default=100,
+        form.addParam('triggerMicrograph', params.IntParam, default=200,
                       label="Micrographs to launch the protocol",
-                      help='Number of micrographs filtered to launch the protocol')
+                      help='Number of micrographs filtered to launch the statistics')
+        form.addParam('binsHist', params.IntParam, default=50,
+                      expertLevel=params.LEVEL_ADVANCED,
+                      label="Number of bins of the Intensity histogram",
+                      help='')
         form.addSection('Streaming')
 
         form.addParam('refreshTime', params.IntParam, default=120,
                       label="Time to refresh data collected (secs)")
 
+        form.addParallelSection(threads=3, mpi=1)
+
+
     def _initialize(self):
         self.movies = self.inputMovies.get()
         self.holes = self.inputHoles.get()
         self.zeroTime = time.time()
+        self.finish = False
 
     def stepsGeneratorStep(self):
         """
@@ -112,26 +121,29 @@ class smartscopeFeedbackFilter(ProtImport, ProtStreamingBase):
         call the self._insertFunctionStep method.
         """
         self._initialize()
-        while True:
-            if len(self.filteredMics.get()) >= self.triggerMicrograph.get():
-                rTime = time.time() - self.zeroTime
-                if rTime >= self.refreshTime.get():
+
+        while not self.finish:
+            rTime = time.time() - self.zeroTime
+            if rTime >= self.refreshTime.get():
+                self.zeroTime = time.time()
+                self.info('len filteredMics: {}'.format(len(self.filteredMics.get())))
+                if len(self.filteredMics.get()) >= self.triggerMicrograph.get():
+                    self.info('In progres...')
+                    self.countStreamingSteps += 1
+
                     self.fMics = self.filteredMics.get()
-                    self.info('collecting...')
-                    getSOH = self._insertFunctionStep(
-                        self.collectSetOfHolesFiltered, prerequisites=[])
-                    statistics = self._insertFunctionStep(
-                        self.statistics,  prerequisites=[getSOH])
-                    feedback = self._insertFunctionStep(
-                        self.postingBack2Smartscope,  prerequisites=[statistics])
-                    createOutput = self._insertFunctionStep(
-                        self.createSetOfFilteredHoles,  prerequisites=[feedback])
+                    self.collectSetOfHolesFiltered()
+                    self.statistics()
+                    self.postingBack2Smartscope()
+                    self.createSetOfFilteredHoles()
                     if not self.fMics.isStreamOpen():
                         self.info('Not more micrographs are expected, set closed')
-                        break
+                        self.finish = True
+                else:
+                    self.info('Waiting enought micrographs to launch protocol.'
+                              ' triggerMicrograph: {}, micrographsFiltered: {}'.format(self.triggerMicrograph.get(), len(self.filteredMics.get())))
 
     def collectSetOfHolesFiltered(self):
-        self.info('in collect')
         self.holesFiltered = []
         dictMovies = {}
         for m in self.movies:
@@ -144,95 +156,134 @@ class smartscopeFeedbackFilter(ProtImport, ProtStreamingBase):
 
 
     def statistics(self):
-        stepNum = 50
         self.info('statistics')
         import numpy as np
-        #import matplotlib.pyplot as plt
         listHoles = []
         listFilteredHoles = []
+        shapiroList = []
         for h in self.holes:
             listHoles.append(h.getSelectorValue())
             if h.getHoleId() in self.holesFiltered:
                 listFilteredHoles.append(h.getSelectorValue())
         arrayHoles = np.array(listHoles)
         arrayFilteredHoles = np.array(listFilteredHoles)
-        #self.info('listFilteredHoles: {}'.format(listFilteredHoles))
         # Calcular los histogramas de ambas series
         minIntensity = min(listHoles)
         maxIntensity = max(listHoles)
-        step = (maxIntensity - minIntensity) / stepNum
-        bins = np.linspace(minIntensity, maxIntensity, stepNum)  # Definir los límites de los bins para el histograma
+        import os
+        fname = "/home/agarcia/Documents/test_JJ.txt"
+        if os.path.exists(fname):
+            os.remove(fname)
+        fjj = open(fname, "a+")
+        fjj.write('JORGE--------->onDebugMode PID {}'.format(os.getpid()))
+        fjj.close()
+        print('JORGE--------->onDebugMode PID {}'.format(os.getpid()))
+        import time
+        time.sleep(10)
+        # JORGE_END
 
-        histTotal, rangeIntensity = np.histogram(arrayHoles, bins=bins)
-        histFiltered, ranges = np.histogram(arrayFilteredHoles, bins=bins)
-        self.info('histTotal:{}\n \nhistFiltered: {}'.format(histTotal, histFiltered))
-        # Asegurarse de que los histogramas tengan el mismo tamaño
-        assert len(histTotal) == len(histFiltered), "Los histogramas no tienen la misma cantidad de bins"
+        listRange = range(5, 101)
+        def calculate(b):
+            step = (maxIntensity - minIntensity) / b
+            bins = np.linspace(minIntensity, maxIntensity, b)  # Definir los límites de los bins para el histograma
+            histTotal, rangeIntensity = np.histogram(arrayHoles, bins=bins)
+            histFiltered, ranges = np.histogram(arrayFilteredHoles, bins=bins)
+            #3self.debug('histTotal:{}\n \nhistFiltered: {}'.format(histTotal, histFiltered))
+            # Asegurarse de que los histogramas tengan el mismo tamaño
+            assert len(histTotal) == len(histFiltered), "Los histogramas no tienen la misma cantidad de bins"
 
-        # Calcular el cociente de los histogramas
-        histRatio = np.divide(histFiltered, histTotal, out=np.zeros_like(histFiltered, dtype=float), where=histTotal != 0)
-        histRatio[np.isinf(histRatio)] = 0
-        histRatio[np.isnan(histRatio)] = 0
-        self.info('ranges: {}'.format(ranges))
-        self.info("histRatio: {}".format(histRatio))
+            # Calcular el cociente de los histogramas
+            histRatio = np.divide(histFiltered, histTotal, out=np.zeros_like(histFiltered, dtype=float), where=histTotal != 0)
+            histRatio[np.isinf(histRatio)] = 0.0
+            histRatio[np.isnan(histRatio)] = 0.0
 
-        mu = np.sum(ranges * histRatio) / np.sum(histRatio)
-        sigma = np.sqrt(np.sum(histRatio * (ranges - mu) ** 2) / np.sum(histRatio))
-        self.minIntensity = mu - sigma
-        self.maxIntensity = mu + sigma + step
-        self.info('std_dev: {}\nmedian_value: {}'.format(sigma, mu))
-        self.info('minIntensity: {}\nmaxIntensity: {}'.format(minIntensity, maxIntensity))
-        # # Graficar los histogramas y el cociente
+            #self.debug('ranges: {}'.format(ranges))
+            #self.debug("histRatio: {}".format(self.histRatio))
 
-        # Plotear los datos
-        # plt.figure(figsize=(10, 6))
-        #
-        # # Scatter plot de los datos originales
-        # plt.scatter(ranges, histRatio, label='Datos', color='b')
-        #
-        # # Generar puntos para la distribución normal
-        # x_fit = np.linspace(np.min(ranges), np.max(ranges), 100)
-        # y_fit = np.exp(-(x_fit - mu) ** 2 / (2 * sigma ** 2)) / (sigma * np.sqrt(2 * np.pi))
-        #
-        # # Plotear la distribución normal
-        # plt.plot(x_fit, y_fit, label=f'Distribución Normal ($\mu$={mu:.2f}, $\sigma$={sigma:.2f})', color='r')
-        #
-        # # Configuración del gráfico
-        # plt.title('Distribución de datos y ajuste a distribución normal')
-        # plt.xlabel('Eje Intensity')
-        # plt.ylabel('Eje Coef')
-        # plt.legend()
-        # plt.grid(True)
-        #
-        # # Mostrar el gráfico
-        # plt.show()
+            mu = np.sum(ranges[:-1] * histRatio) / np.sum(histRatio)
+            sigma = np.sqrt(np.sum(histRatio * (ranges[:-1] - mu) ** 2) / np.sum(histRatio))
+            minIntensityL = int(mu - sigma)
+            maxIntensityL = int(mu + sigma + step)
+            #self.info('std_dev: {}\nmedian_value: {}'.format(sigma, mu))
+            #self.info('minIntensity: {}\minIntensityL: {}'.format(self.minIntensity, self.maxIntensityL))
+            #saving data to plot in extra folder
+            rangeFile = self._getExtraPath("rangeI-{}.txt".format(self.countStreamingSteps))
+            np.savetxt(rangeFile, rangeIntensity[:-1].reshape(1, -1), fmt='%.8f', delimiter=' ')
+            histRatioFile = self._getExtraPath("histRatio-{}.txt".format(self.countStreamingSteps))
+            np.savetxt(histRatioFile, histRatio.reshape(1, -1), fmt='%.8f', delimiter=' ')
+            shapiroTest = self.Shapiro_Wilk(histRatio)
+            shapiroList.append(shapiroTest[1])
+            return mu, sigma, minIntensityL, maxIntensityL
+
+        for b in listRange: #To check with prints the best bin value
+            _, _, _, _ = calculate(b)
+
+        self.info('best bin to normal distribution: {}'.format(listRange[np.argmax(shapiroList)]))
+        mu, sigma, minIntensityL, maxIntensityL = calculate(listRange[np.argmax(shapiroList)])
+        # SUMMARY INFO
+        summaryF = self._getExtraPath("summary.txt")
+        summaryF = open(summaryF, "w")
+        summaryF.write('\n\nStandard deviation: {}\nMedian value: {}\n'.format(int(sigma), int(mu)) +
+                       'Intensity range with holes to acquire: {} - {}'.format(minIntensityL, maxIntensityL))
+        summaryF.close()
+
+    def Shapiro_Wilk(self, histRatio):
+        # Test de Shapiro-Wilk
+        stat, p_value = shapiro(histRatio)
+        print(f"Shapiro-Wilk test: W={stat:.4f}, p-value={p_value:.4f}")
+        # Interpretation Shapiro-Wilk test
+        alpha = 0.05
+        if p_value > alpha:
+            return True, p_value, "Pass the null hypothesis of Shapiro_Wilk (the data appears to be normally distributed)."
+        else:
+            return False, p_value, "Reject the null hypothesis of Shapiro_Wilk ({} < {}) (the data does not appear to be normally distributed).".format(
+                p_value, alpha)
 
     def postingBack2Smartscope(self):
-        for h in self.holes:
-            if self.minIntensity > h.getSelectorValue() or h.getSelectorValue() > self.maxIntensity and h.getStatus() != 'completed':
-                self.pyClient.postParameterFromID('holes', h.getHoleId(), data={"status": 'Cancelled'})
+        # TODO post a setValueIntensity
+        # TODO request regroup
+        # TODO requeue
+        # for h in self.holes:
+        #     if self.minIntensity > h.getSelectorValue() or h.getSelectorValue() > self.maxIntensity and h.getStatus() != 'complete':
+        #         self.pyClient.postParameterFromID('holes', h.getHoleId(), data={"selected": 'False'})
+        pass
 
     def createSetOfFilteredHoles(self):
-        SOH = SetOfHoles.create(outputPath=self._getPath())
-        self.outputsToDefine = {'SetOfHoles': SOH}
+        SOH = SetOfHoles.create(outputPath=self._getPath())#baseName
+        SOHFO = SetOfHoles.create(outputPath=self._getPath(), prefix='FilteredOut')
+        self.outputsToDefine = {'SetOfHolesFiltered': SOH, 'SetOfHolesFilteredOut': SOHFO}
         self._defineOutputs(**self.outputsToDefine)
         for h in self.holes:
             if h.getHoleId() in self.holesFiltered:
-                self.createOutputStep(SOH, h, self.holes)
+                self.createOutputStep(SOH, h)
+            else:
+                self.createOutputStepFilteredOut(SOHFO, h)
 			    
-    def createOutputStep(self, SOH, hole, holes):
-        SOH.copyInfo(holes)
+    def createOutputStep(self, SOH, hole):
+        SOH.copyInfo(self.holes)
         hole2Add_copy = Hole()
         hole2Add_copy.copy(hole, copyId=False)
         SOH.append(hole2Add_copy)
-        
-        if self.hasAttribute('SetOfHoles'):
+        if self.hasAttribute('SetOfHolesFiltered'):
             SOH.write()
-            outputAttr = getattr(self, 'SetOfHoles')
+            outputAttr = getattr(self, 'SetOfHolesFiltered')
             outputAttr.copy(SOH, copyId=False)
             self._store(outputAttr)
         # STORE SQLITE
         self._store(SOH)
+
+    def createOutputStepFilteredOut(self, SOHFO, hole):
+        SOHFO.copyInfo(self.holes)
+        hole2Add_copy = Hole()
+        hole2Add_copy.copy(hole, copyId=False)
+        SOHFO.append(hole2Add_copy)
+        if self.hasAttribute('SetOfHolesFilteredOut'):
+            SOHFO.write()
+            outputAttr = getattr(self, 'SetOfHolesFilteredOut')
+            outputAttr.copy(SOHFO, copyId=False)
+            self._store(outputAttr)
+        # STORE SQLITE
+        self._store(SOHFO)
 				    
     def checkSmartscopeConnection(self):
         response = self.pyClient.getDetailsFromParameter('users')
@@ -241,8 +292,14 @@ class smartscopeFeedbackFilter(ProtImport, ProtStreamingBase):
 
     def _summary(self):
         summary = []
-
-
+        summaryF = self._getExtraPath("summary.txt")
+        if not os.path.exists(summaryF):
+            summary.append("No summary file yet.")
+        else:
+            summaryF = open(summaryF, "r")
+            for line in summaryF.readlines():
+                summary.append(line.rstrip())
+            summaryF.close()
         return summary
 
     def _validate(self):
--- smartscope/viewers/viewer_data.py
+++ smartscope/viewers/viewer_data.py
@@ -30,12 +30,14 @@
 from pwem.viewers.viewers_data import DataViewer
 #from ..objects.data_deprecated import *
 from ..objects.data import *
-from pwem.viewers import DataView, ObjectView
+from pwem.viewers import DataView, ObjectView, EmPlotter
 from pwem.viewers.showj import ORDER, VISIBLE, MODE, RENDER, MODE_MD, ZOOM, SORT_BY
 from pwem.viewers.showj import *
 from pyworkflow.viewer import DESKTOP_TKINTER, WEB_DJANGO, ProtocolViewer
 from smartscope.protocols.protocol_feedback_filter import smartscopeFeedbackFilter
 from pyworkflow.protocol.params import IntParam, LabelParam
+import numpy as np
+import matplotlib.pyplot as plt
 
 # class DataViewer_cnb(DataViewer):
 #     _targets = [SetOfLowMagImages]
@@ -47,8 +49,8 @@ from pyworkflow.protocol.params import IntParam, LabelParam
 
 class DataViewer_smartscope(DataViewer):
     _targets = [SetOfGrids, SetOfAtlas, SetOfSquares, SetOfHoles, SetOfMoviesSS]
-
     def _visualize(self, obj, **kwargs):
+        self._views = []
         if isinstance(obj, SetOfSquares):
             labels = ('_pngDir _square_id _atlas_id _status _selected _completion_time _area _shape_x _shape_y _sampligRate')
             self._views.append(ObjectView(self._project,
@@ -103,8 +105,8 @@ class SmartscopeFilterFeedbackViewer(ProtocolViewer):
     def _defineParams(self, form):
         form.addSection(label='Visualization')
         group = form.addGroup('Holes')
-        group.addParam('visualizeAllHoles', LabelParam,
-                       label="Visualize all holes",
+        group.addParam('visualizeFilteredOutHoles', LabelParam,
+                       label="Visualize filtered out holes",
                        help="")
         group.addParam('visualizeFilteredHoles', LabelParam,
                        label="Visualize filtered holes",
@@ -112,16 +114,120 @@ class SmartscopeFilterFeedbackViewer(ProtocolViewer):
         group2 = form.addGroup('Statistics')
         group2.addParam('visualizeHistograms', LabelParam,
                        label="Visualize the histograms of intensity",
-                       help="Visualize the histograms of intensity per holes")
+                       help="Visualize the histograms of intensity per holes. The last serie"
+                            " is scattered and the first (the older) and the last (newest) 5 gaussian reconstructions")
 
 
     def _getVisualizeDict(self):
         return {
-                 'visualizeAllHoles': self._visualizeAllHoles,
+                 'visualizeFilteredOutHoles': self._visualizeFilteredOut,
                  'visualizeFilteredHoles': self._visualizeFilteredHoles,
                  'visualizeHistograms': self._visualizeHistograms,
                 }
 
-    def _visualizeAllHoles(self, e=None):
-        return self._visualizeCtfs("outputCTF")
 
+    def _visualizeFilteredHoles(self, e=None):
+        views = []
+        if hasattr(self.protocol, 'SetOfHolesFiltered'):
+            labels = (
+                '_pngDir _hole_id _grid_id _status _selected _completion_time _shape_x _shape_y _sampligRate _number _area')
+            views.append(ObjectView(self._project,
+                                    self.protocol.SetOfHolesFiltered.strId(),
+                                    self.protocol.SetOfHolesFiltered.getFileName(),
+                                    viewParams={VISIBLE: labels,
+                                                RENDER: '_pngDir',
+                                                SORT_BY: labels}))
+            return views
+
+    def _visualizeFilteredOut(self, e=None):
+        views = []
+        if hasattr(self.protocol, 'SetOfHolesFilteredOut'):
+            labels = (
+                '_pngDir _hole_id _grid_id _status _selected _completion_time _shape_x _shape_y _sampligRate _number _area')
+            views.append(ObjectView(self._project,
+                                          self.protocol.SetOfHolesFilteredOut.strId(),
+                                          self.protocol.SetOfHolesFilteredOut.getFileName(),
+                                          viewParams={VISIBLE: labels,
+                                                      RENDER: '_pngDir',
+                                                      SORT_BY: labels}))
+            return views
+
+
+    def _visualizeHistograms(self, e=None):
+        def muSigma(intensityRange, coefHoles):
+            mu = np.sum(intensityRange * coefHoles) / np.sum(coefHoles)
+            sigma = np.sqrt(np.sum(coefHoles * (intensityRange - mu) ** 2) / np.sum(coefHoles))
+            return mu, sigma
+        listRangesFiles = []
+        listHistFiles = []
+        listRanges = []
+        listHist = []
+        files = os.listdir(self.protocol._getExtraPath())
+        with open(os.path.join(self.protocol._getExtraPath(),'pathsFile.txt'), 'w') as fi:
+            for f in files:
+                fi.write(f)
+                fi.write('\n')
+
+        for f in files:
+            if f.find('rangeI') != -1:
+                listRangesFiles.append(f)
+            elif f.find('histRatio') != -1:
+                listHistFiles.append(f)
+
+        def extraer_numero(rango):
+            return int(rango.split('-')[1].split('.')[0])
+
+        # Ordenar la lista utilizando la función de clave personalizada
+        listRangesFiles = sorted(listRangesFiles, key=extraer_numero)[::-1]
+        listHistFiles = sorted(listHistFiles, key=extraer_numero)[::-1] #the first the newer
+        for f in listRangesFiles:
+            listRanges.append(np.loadtxt(os.path.join(self.protocol._getExtraPath(), f), dtype=np.float))
+        for f in listHistFiles:
+            listHist.append(np.loadtxt(os.path.join(self.protocol._getExtraPath(), f), dtype=np.float))#TODO no carga datos del fichero
+
+        mu = []
+        sigma = []
+        if len(listRangesFiles) > 0:
+            for index, _ in enumerate(listRangesFiles):
+                if len(listHist) > 0:
+                    m, s = muSigma(listRanges[index], listHist[index])
+                    mu.append(m)
+                    sigma.append(s)
+
+        # Crear figura y ejes
+        fig, ax1 = plt.subplots(figsize=(12, 8))
+        # Calcular el ancho de los bins
+        bin_widths = int((listRanges[0][1] - listRanges[0][0])) #truncated number
+
+        # Configuración del primer eje (izquierdo) para los puntos
+        color = 'tab:blue'
+        ax1.set_xlabel('Holes Intensity')
+        ax1.set_ylabel('total holes / good holes')
+        ax1.bar(listRanges[0], listHist[0], label='Coef good holes (Last update)', color='midnightblue', width=bin_widths, alpha=0.8, edgecolor='black')
+        ax1.tick_params(axis='y')
+        ax1.legend(loc='upper left')
+        ax1.set_ylim(0, 1)
+        ax1.grid(True)
+
+        # Crear el segundo eje (derecho) para la distribución normal
+        ax2 = ax1.twinx()
+        color = 'black'
+        ax2.set_ylabel('Gaussian Distribution', color=color)
+        numSerie2 = 0
+        colors = ['midnightblue', 'mediumblue', 'slateblue', 'mediumpurple']
+        order = ['Last', 'Second to last', 'Third to last', 'Fourth to last']
+        for _ in listRanges[:4]:
+            x_fit = np.linspace(np.min(listRanges[numSerie2]), np.max(listRanges[numSerie2]), 100)
+            y_fit1 = np.exp(-(x_fit - mu[numSerie2]) ** 2 / (2 * sigma[numSerie2] ** 2)) / (sigma[numSerie2] * np.sqrt(2 * np.pi))
+            ax2.plot(x_fit, y_fit1, label=f'{order[numSerie2]} ($\mu$={mu[numSerie2]:.2f}, $\sigma$={sigma[numSerie2]:.2f})',
+                     color=colors[numSerie2])
+
+            ax2.tick_params(axis='y', labelcolor=color)
+            ax2.legend(loc='upper right')
+            numSerie2 += 1
+        ax2.fill_between(x_fit, 0, y_fit1, where=((x_fit >= mu[0] - sigma[0]) & (x_fit <= mu[0] + sigma[0])),
+                         color='green', alpha=0.3, label=f'Rango $\mu \pm \sigma$')
+        ax2.set_ylim(0, max(y_fit1) * 1.1)
+
+        plt.title('Intensity - total / good holes')
+        plt.show()
